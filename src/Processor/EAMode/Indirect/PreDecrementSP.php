1f53d9bf18ed6daf82e9e1973cb9bfadc248a521
0xABADCAFE <karlchurchill@gmail.com>
2025-09-13 19:15:38 +0100

    Added special case handling for predecrement/postincrement modes for A7 to ensure word alignment on byte accesses and fixed some EA setup bugs that were using the data registers and not the address registers.

---
+++ src/Processor/EAMode/Indirect/PreDecrementSP.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *       _/_/_/    _/_/    _/_/_/   _/    _/  _/_/_/   _/                                                            _/
+ *     _/       _/    _/  _/    _/ _/    _/  _/    _/ _/_/_/     _/_/   _/    _/   _/_/_/    _/_/_/  _/_/_/     _/_/_/
+ *    _/_/_/     _/_/    _/_/_/   _/_/_/_/  _/_/_/   _/    _/ _/    _/ _/    _/ _/_/      _/    _/  _/    _/ _/    _/
+ *   _/    _/ _/    _/  _/       _/    _/  _/       _/    _/ _/    _/ _/    _/     _/_/  _/    _/  _/    _/ _/    _/
+ *    _/_/     _/_/    _/       _/    _/  _/       _/    _/   _/_/    _/_/_/  _/_/_/      _/_/_/  _/    _/   _/_/_/
+ *
+ *   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Damn you, linkedin, what have you started ? <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+ */
+
+declare(strict_types=1);
+
+namespace ABadCafe\G8PHPhousand\Processor\EAMode\Indirect;
+use ABadCafe\G8PHPhousand\Processor\EAMode;
+use ABadCafe\G8PHPhousand\Processor\ISize;
+use ABadCafe\G8PHPhousand\Device;
+use ABadCafe\G8PHPhousand\Processor;
+
+use ValueError;
+
+/**
+ * Variant of PreDecrement that is used for the 68000 stack pointer. Byte sized
+ * accesses always result in word sized adjustments.
+ */
+class PreDecrementSP extends PreDecrement
+{
+    /**
+     * @return int<0,255>
+     */
+    public function readByte(): int
+    {
+        $this->iAddress = $this->iRegister = ($this->iRegister - ISize::WORD) & ISize::MASK_LONG;
+        return $this->oOutside->readByte($this->iAddress);
+    }
+
+
+    /**
+     * @param int<0,255> $iValue
+     */
+    public function writeByte(int $iValue): void
+    {
+        $iAddress = $this->iAddress;
+        if (null === $iAddress) {
+            $iAddress = $this->iRegister = ($this->iRegister - ISize::WORD) & ISize::MASK_LONG;
+        }
+        $this->oOutside->writeByte($iAddress, $iValue);
+        $this->iAddress = null;
+    }
+
+}
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#php -dzend.assertions=1 test_memory.php
-#php -dzend.assertions=1 test_regs.php
-#php -dzend.assertions=1 test_eamodes.php
+php -dzend.assertions=1 test_memory.php
+php -dzend.assertions=1 test_regs.php
+php -dzend.assertions=1 test_eamodes.php
 php -dzend.assertions=1 test_op_direct.php
-#php -dzend.assertions=1 test_scc.php
+php -dzend.assertions=1 test_scc.php
