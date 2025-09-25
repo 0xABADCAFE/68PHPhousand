5bb1a0c4342c93e339aabba41cb324f0e17967cd
0xABADCAFE <karlchurchill@gmail.com>
2025-09-25 20:28:04 +0100

    MVP ROL

--- src/templates/operation/logic/rol_imm.tpl.php
+++ src/templates/operation/logic/rol_imm.tpl.php
@@ -3,14 +3,65 @@
 /**
  * LSL #d,dN
  *
+ * X and C are set according to the last bit shifted out
+ *
  */
 use ABadCafe\G8PHPhousand\Processor\IOpcode;
 use ABadCafe\G8PHPhousand\Processor\Opcode\ILogical;
 
 assert(!empty($oParams), new \LogicException());
 
-// TODO
+$iSize      = $oParams->iOpcode & IOpcode::MASK_OP_SIZE;
+$iImmediate = (($oParams->iOpcode & IOpcode::MASK_IMM_SMALL) >> IOpcode::REG_UP_SHIFT);
+if (0 === $iImmediate) {
+    $iImmediate = 8;
+}
+$iReg = $oParams->iOpcode & IOpcode::MASK_EA_REG;
+
 ?>
 return function(int $iOpcode): void {
+    $this->iConditionRegister &= IRegister::CCR_CLEAR_XCV;
+<?php
 
+switch ($iSize) {
+    case IOpcode::OP_SIZE_B:
+?>
+    $iValue = ($this->oDataRegisters->iReg<?= $iReg ?> & ISize::MASK_BYTE) << <?= $iImmediate ?>;
+    $iValue |= ($iValue >> 8);
+    $this->updateNZByte($iValue);
+    $this->iConditionRegister |= (
+        ($iValue & 0x100) ? IRegister::CCR_MASK_XC : 0
+    );
+    $this->oDataRegisters->iReg<?= $iReg ?> &= ISize::MASK_INV_BYTE;
+    $this->oDataRegisters->iReg<?= $iReg ?> |= ($iValue & ISize::MASK_BYTE);
+<?php
+    break;
+
+    case IOpcode::OP_SIZE_W:
+?>
+    $iValue = ($this->oDataRegisters->iReg<?= $iReg ?> & ISize::MASK_WORD) << <?= $iImmediate ?>;
+    $iValue |= ($iValue >> 16);
+    $this->updateNZWord($iValue);
+    $this->iConditionRegister |= (
+        ($iValue & 0x10000) ? IRegister::CCR_MASK_XC : 0
+    );
+    $this->oDataRegisters->iReg<?= $iReg ?> &= ISize::MASK_INV_WORD;
+    $this->oDataRegisters->iReg<?= $iReg ?> |= ($iValue & ISize::MASK_WORD);
+<?php
+    break;
+
+    case IOpcode::OP_SIZE_L:
+?>
+    $iValue = ($this->oDataRegisters->iReg<?= $iReg ?> & ISize::MASK_LONG) << <?= $iImmediate ?>;
+    $iValue |= ($iValue >> 32);
+    $this->updateNZLong($iValue);
+    $this->iConditionRegister |= (
+        ($iValue & 0x100000000) ? IRegister::CCR_MASK_XC : 0
+    );
+    $this->oDataRegisters->iReg<?= $iReg ?> = ($iValue & ISize::MASK_LONG);
+<?php
+    break;
+
+}
+?>
 };
